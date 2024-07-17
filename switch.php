47946055b0353275c75eaa188a8fe3409f2d0a83
jardison2021 <jardisonrs@live.com>
2024-07-16 22:12:39 -0300

    Switch

---
+++ switch.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Switch</title>
+</head>
+<body>
+	<?php 
+		$parametro = 2;
+		switch ($parametro) {
+			case 1:
+				echo 'Entrou no case 1';
+				break;
+			case 2:
+				echo 'Entrou no case 2';
+				break;
+			case 3:
+				echo 'Entrou no case 3';
+				break;			
+			default:
+				echo 'Entrou no default'
+				break;
+		}
+	?>
+</body>
+</html>
