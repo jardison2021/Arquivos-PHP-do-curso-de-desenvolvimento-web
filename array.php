4bc42fe6a1d264568a52d4ca120bb6c943720093
jardison2021 <jardisonrs@live.com>
2024-07-25 22:18:14 -0300

    Array multidimensional

--- array.php
+++ array.php
@@ -45,5 +45,21 @@
 		echo '</pre>';
 		echo $lista_frutas2['f'];
 	?>
+	<?php
+		//Arrays mltidimensionais
+		echo '<h1>Arrays multidimensionais</h1>';
+		echo '<p>Array mltidimensional é uma série de arrays dentro de outro array</p>';
+		echo '<h3>Array associativo</h3>';
+		$lista_coisas = [];
+		$lista_coisas['frutas'] = [1 => 'Banana', 2 => 'Maçã', 3 => 'Morango', 4 => 'Uva'];
+		$lista_coisas['pessoas'] = [1 => 'João', 2 => 'José', 3 => 'Maria'];
+		echo '<pre>';
+		print_r($lista_coisas);
+		echo '</pre>';
+		echo '<hr>';
+		echo 'Recuperando o valor no índice 3 do array frutas: ' . $lista_coisas['frutas'][3];
+		echo '<br>';
+		echo 'Recuperando o valor no índice 2 do array pessoas: ' . $lista_coisas['pessoas'][2];
+	?>
 </body>
 </html>
