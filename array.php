50c2aabdf0f271b8936e9c6d36aa1a84a8327e2d
jardison2021 <jardisonrs@live.com>
2024-07-28 21:13:29 -0300

    Métodos de pesquisa em array - in_array() e array_search()

--- array.php
+++ array.php
@@ -47,6 +47,7 @@
 	?>
 	<?php
 		//Arrays mltidimensionais
+		echo '<hr>';
 		echo '<h1>Arrays multidimensionais</h1>';
 		echo '<p>Array mltidimensional é uma série de arrays dentro de outro array</p>';
 		echo '<h3>Array associativo</h3>';
@@ -60,6 +61,52 @@
 		echo 'Recuperando o valor no índice 3 do array frutas: ' . $lista_coisas['frutas'][3];
 		echo '<br>';
 		echo 'Recuperando o valor no índice 2 do array pessoas: ' . $lista_coisas['pessoas'][2];
+	?>
+	<?php
+		//Métodos de pesquisa em array
+		//in_array() - retorna true ou false para existência do que esta sendo pesquisado
+		//array_search() - retorna o índice do valor pesquisado, caso ele exista
+		echo '<hr>';
+		echo '<h1>Métodos de pesquisa em arrays</h1>';
+		$lista_frutas = ['Banana', 'Maçã','Morango', 'Uva',];
+		echo '<pre>';
+		print_r($lista_frutas);
+		echo '</pre>';
+		echo '<h3>Método de pesquisa in_array()</h3>';
+		echo '<p>Esse método retorna true ou false para existência do que está sendo pesquisado</p>';
+		$retorno = in_array('Maçãs', $lista_frutas);
+		if($retorno){
+			echo 'Objeto ' . $lista_frutas[1] . ' encontrado';
+		}else{
+			echo 'Objeto não encontrado';
+		}
+		echo '<h3>Método de pesquisa array_search()</h3>';
+		echo '<p>Esse método retorna índice do valor pesquisado, caso ele exista</p>';
+		$retorno2 = array_search('Uva', $lista_frutas);
+		if($retorno2){
+			echo 'Objeto ' . $retorno2 . ' encontrado';
+		}else{
+			echo 'Objeto não encontrado';
+		}
+		echo '<h3>Método de pesquisa em array multidimensional</h3>';
+		echo '<h3>in_array()</h3>';
+		$lista_coisas = ['frutas' => $lista_frutas, 'pessoas' => ['Francisco', 'Jardison']];
+		echo '<pre>';
+		print_r($lista_coisas);
+		echo '</pre>';
+		$retorno3 = in_array('Uva', $lista_coisas['frutas']);
+		if($retorno3){
+			echo 'Objeto encontrado';
+		}else{
+			echo 'Objeto não encontrado';
+		}
+		echo '<h3>array_search()</h3>';
+		$retorno4 = array_search('Uva', $lista_coisas['frutas']);
+		if($retorno4){
+			echo 'Objeto ' . $retorno4 . ' encontrado';
+		}else{
+			echo 'Objeto não encontrado';
+		}
 	?>
 </body>
 </html>
