e2227bf37e6af6eb889222e24cbb463ecdbeb070
jardison2021 <jardisonrs@live.com>
2024-08-04 16:30:51 -0300

    Praticando um pouco mais com Whille, Do Whille e For

--- estruturas_de_repeticao.php
+++ estruturas_de_repeticao.php
@@ -14,6 +14,11 @@
 		h3{
 			color: #90d4d0;
 		}
+		#titulo{
+			background: #1E90FF;
+			border-radius: 5px;
+			color: white;
+		}
 	</style>
 </head>
 <body>
@@ -61,6 +66,48 @@
 	<?php
 		echo '<h3>Foreach</h3>';
 		echo '<p>Executa o loop sobre os elementos de um array.</p>';
+		echo '<hr>';
+	?>
+	<?php
+		echo '<h1>Praticando um pouco mais, percorrendo arrays com Whille, Do Whille e For</h1>';
+		echo '<h3>Whille</h3>';
+		$registros = array(array('titulo' => 'Título notícia 1', 'conteudo' => 'Conteúdo notícia 1'), array('titulo' => 'Título notícia 2', 'conteudo' => 'Conteúdo notícia 2'), array('titulo' => 'Título notícia 3', 'conteudo' => 'Conteúdo notícia 3'), array('titulo' => 'Título notícia 4', 'conteudo' => 'Conteúdo notícia 4'));
+		echo '<pre>';
+		print_r($registros);
+		echo '</pre>';
+		$idx = 0;
+		while($idx < count($registros)){
+			echo '<h3 id="titulo">' . $registros[$idx]['titulo'] . '</h3>' ;
+			echo '<p>' . $registros[$idx]['conteudo'] . '</p>' ;
+			echo '<hr>';
+			$idx++;
+		}
+	?>
+	<?php
+		echo '<h3>Do Whille</h3>';
+		$registros = array(array('titulo' => 'Título notícia 1', 'conteudo' => 'Conteúdo notícia 1'), array('titulo' => 'Título notícia 2', 'conteudo' => 'Conteúdo notícia 2'), array('titulo' => 'Título notícia 3', 'conteudo' => 'Conteúdo notícia 3'), array('titulo' => 'Título notícia 4', 'conteudo' => 'Conteúdo notícia 4'));
+		echo '<pre>';
+		print_r($registros);
+		echo '</pre>';
+		$idx = 0;
+		do{
+			echo '<h3 id="titulo">' . $registros[$idx]['titulo'] . '</h3>' ;
+			echo '<p>' . $registros[$idx]['conteudo'] . '</p>' ;
+			echo '<hr>';
+			$idx++;
+		}while($idx < count($registros))
+	?>
+	<?php
+		echo '<h3>For</h3>';
+		$registros = array(array('titulo' => 'Título notícia 1', 'conteudo' => 'Conteúdo notícia 1'), array('titulo' => 'Título notícia 2', 'conteudo' => 'Conteúdo notícia 2'), array('titulo' => 'Título notícia 3', 'conteudo' => 'Conteúdo notícia 3'), array('titulo' => 'Título notícia 4', 'conteudo' => 'Conteúdo notícia 4'));
+		echo '<pre>';
+		print_r($registros);
+		echo '</pre>';
+		for($idx = 0; $idx < count($registros); $idx++){
+			echo '<h3 id="titulo">' . $registros[$idx]['titulo'] . '</h3>' ;
+			echo '<p>' . $registros[$idx]['conteudo'] . '</p>' ;
+			echo '<hr>';
+		}
 	?>
 </body>
 </html>
