8968d9cc4686bf55823deb412bc3d2054bbe01a2
jardison2021 <jardisonrs@live.com>
2024-10-23 20:29:28 -0300

    OO - pilar encapsulamento - parte 2

--- php_oo/oo_pilar_encapsulamento.php
+++ php_oo/oo_pilar_encapsulamento.php
@@ -3,6 +3,7 @@
 		private $nome = 'Jardison';
 		protected $sobrenome = 'Rebouças';
 		public $humor = 'Feliz';
+		
 		public function __get($attr){
 			return $this->$attr;
 		}
@@ -33,8 +34,38 @@
 			}
 		}*/
 	}
+	class Filho extends Pai{
+		/*public function getAtributo($attr){
+			return $this->$attr;
+		}
+		public function setAtributo($attr, $valor){
+			$this->$attr = $valor;
+		}*/
+		/*public function __get($attr){
+			return $this->$attr;
+		}
+		public function __set($attr, $value){
+			$this->$attr = $value;
+		}*/
+		public function __construct(){
+			//exibir os métodos do objeto
+			echo '<pre>';
+			print_r(get_class_methods($this));
+			echo '</pre>';
+		}
+		public function executarMania(){
+			echo 'Cantar';
+		}
+		public function x(){
+			$this->executarMania();
+		}
+		protected function responder(){
+			echo 'Olá!';
+		}
+	}
+	/*
 	$pai = new Pai();
-	echo $pai->executarAcao();
+	echo $pai->executarAcao();*/
 	//echo $pai->humor;s
 	/*echo $pai->sobrenome;
 	echo $pai->sobrenome = 'Santos';
@@ -44,4 +75,29 @@
 	$pai->setNome('Francisco');
 	echo '<br>';
 	echo $pai->getNome();*/
+	$filho = new Filho();
+	echo '<pre>';
+	print_r($filho);
+	echo '</pre>';
+	$filho->executarAcao();
+	echo '<br>';
+	$filho->x();
+
+	/*
+	echo $filho->__get('nome');
+	echo '<br>';
+	echo $filho->__set('nome', 'Francisco');
+	echo $filho->__get('nome');
+	echo '<pre>';
+	print_r($filho);
+	echo '</pre>';
+
+	echo $filho->getAtributo('nome');
+	echo $filho->setAtributo('nome', 'Francisco');
+	echo '<pre>';
+	print_r($filho);
+	echo '</pre>';
+	echo '<br>';
+	echo $filho->getAtributo('nome');
+	*/
 ?>
