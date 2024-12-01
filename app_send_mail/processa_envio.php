3097658ecc7421026f26c694c3a9c5d47b8a99a9
jardison2021 <jardisonrs@live.com>
2024-12-01 18:25:11 -0300

    App Send Email - Parte 5

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -14,6 +14,7 @@
 		private $para = null;
 		private $assunto = null;
 		private $mensagem = null;
+		public $status = array('codigo_status' => null, 'descricao_status' => '');
 		//métodos
 		public function __get($atributo){
 			return $this->$atributo;
@@ -35,19 +36,19 @@
 	//print_r($mensagem);
 	if(!$mensagem->mensagemValida()){
 		echo 'Mensagem inválida!';
-		die();
+		header('Location: index.php');
 	}
 
 	$mail = new PHPMailer(true);
 
 	try {
 	    //Server settings
-	    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+	    $mail->SMTPDebug = false;                      //Enable verbose debug output
 	    $mail->isSMTP();                                            //Send using SMTP
 	    $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
 	    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-	    $mail->Username   = 'reboucasfrancisco84@gmail.com';        //SMTP username
-	    $mail->Password   = '';                             //SMTP password
+	    $mail->Username   = '******';        //SMTP username
+	    $mail->Password   = '******';                             //SMTP password
 	    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
 	    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
@@ -70,8 +71,49 @@
 	    $mail->AltBody = 'É necessário utilizar um client que suporte HTML para ter acesso ao conteúdo dessa mensagem.';
 
 	    $mail->send();
-	    echo 'E-mail enviado com sucesso!';
+	    $mensagem->status['codigo_status'] = 1;
+	    $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso!';
 	} catch (Exception $e) {
-	    echo "Não foi possível enviar a mensagem, por favor, tente novamente mais tarde. Detalhes do erro: {$mail->ErrorInfo}";
+		$mensagem->status['codigo_status'] = 2;
+	    $mensagem->status['descricao_status'] = 'Não foi possível enviar a mensagem, por favor, tente novamente mais tarde. Detalhes do erro: ' . $mail->ErrorInfo;
 	}
 ?>
+<!-- Método 1 -->
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8" />
+    	<title>App Mail Send</title>
+
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="container">
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<?php if($mensagem->status['codigo_status'] == 1){ ?>
+							<div class="container">
+								<h1 class="display-4 text-success">Sucesso</h1>
+								<p><?= $mensagem->status['descricao_status'] ?></p>
+								<a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+							</div>
+					<?php }?>
+				</div>
+				<div class="col-md-12">
+					<?php if($mensagem->status['codigo_status'] == 2){ ?>
+						<div class="container">
+							<h1 class="display-4 text-danger">Falha</h1>
+							<p><?= $mensagem->status['descricao_status'] ?></p>
+							<a href="index.php" class="btn btn-danger btn-lg mt-5 text-white">Voltar</a>
+						</div>
+					<?php }?>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
