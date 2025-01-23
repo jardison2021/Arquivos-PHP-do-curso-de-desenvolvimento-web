626ede5a14c17abdb98b716ddc178e10e270987a
jardison2021 <jardisonrs@live.com>
2025-01-23 20:47:20 -0300

    Tratando exception (PDO EXCEPTION) - tratamento de erros

--- php_com_pdo/index.php
+++ php_com_pdo/index.php
@@ -2,5 +2,11 @@
 	$dsn = 'mysql:host=localhost;dbname=php_com_pdo';
 	$usuario = 'root';
 	$senha = '';
-	$conexao = new PDO($dsn, $usuario, $senha);
+	//Tratamento de erros
+	try{
+		$conexao = new PDO($dsn, $usuario, $senha);
+	}catch(PDOException $e){
+		echo 'Erro: '.$e->getcode().' Mensagem: '.$e->getMessage();
+		//registrar erro
+	}
 ?>
